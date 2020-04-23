aa050b96879befd773f8e55904c9f6272f78b3d8
Trevor Apple <trappleco@gmail.com>
2020-04-22 23:20:40 -0700

    fixed compiler warning for switch statements

--- code/build.bat
+++ code/build.bat
@@ -5,18 +5,20 @@ REM TODO - can we just build both with one exe?
 call .\setup.bat
 
 set BuildFolder=..\build
-set CommonCompilerFlags=-MTd -nologo -GR- -EHa- -Oi -W4 -FC -Z7
+set CommonCompilerFlags=-MTd -nologo -GR- -EHa- -Oi -Od -W4 -FC -Z7 -w44062
 set CommonLinkerFlags=-opt:ref user32.lib gdi32.lib winmm.lib
 
 IF NOT EXIST %BuildFolder% mkdir %BuildFolder%
 pushd %BuildFolder%
 del *.pdb > nul 2> nul
 rem NOTE: Not using WX because I want to keep the 4100 warning but it will
-rem NOTE: always be hit on the WinMain callback 
-rem NOTE: Disabled exception handling
-rem NOTE: Disabled runtime type information
+rem CONT: always be hit on the WinMain callback 
+rem NOTE: -w44062 is here because, for some reason, it is not seen by the 
+rem CONT: level 4 warnings
+rem NOTE: -EHA- Disabled exception handling
+rem NOTE: -GR- Disabled runtime type information
 rem NOTE: -MT is used so that we don't use the c-runtime library DLL but 
-rem NOTE: statically link instead 
+rem CONT: statically link instead 
 rem NOTE: -Od should be removed for non-debug builds
 cl %CommonCompilerFlags% ..\code\win32_main.cpp -Fmwin32_main.map /link %CommonLinkerFlags%
 popd

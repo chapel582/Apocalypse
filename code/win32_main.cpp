00c9355e036cc02478ab6311e21be8c5014ed9d1
Trevor Apple <trappleco@gmail.com>
2020-04-23 15:22:53 -0700

    added window for windows

--- code/win32_main.cpp
+++ code/win32_main.cpp
@@ -1,5 +1,68 @@
 #include <windows.h>
 
+bool GlobalRunning = false;
+
+LRESULT CALLBACK MainWindowCallback(
+	HWND Window,
+	UINT Message,
+	WPARAM WParam,
+	LPARAM LParam
+)
+{
+	LRESULT Result = 0;
+	switch(Message)
+	{
+		case(WM_SIZE):
+		{
+			OutputDebugStringA("WM_SIZE\n");
+			break;
+		}
+		case(WM_ACTIVATEAPP):
+		{
+			OutputDebugStringA("WM_ACTIVATEAPP\n");
+			break;
+		}
+		case(WM_CLOSE):
+		{
+			GlobalRunning = false;
+			break;
+		}
+		case(WM_DESTROY):
+		{
+			GlobalRunning = false;
+			break;
+		}
+		case(WM_PAINT):
+		{
+			PAINTSTRUCT Paint = {};
+			HDC DeviceContext = BeginPaint(Window, &Paint);
+			int X = Paint.rcPaint.left;
+			int Y = Paint.rcPaint.top;
+			int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+			int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+			static DWORD Operation = WHITENESS;
+			PatBlt(DeviceContext, X, Y, Width, Height, Operation);
+			if(Operation == WHITENESS)
+			{
+				Operation = BLACKNESS;
+			}
+			else
+			{
+				Operation = WHITENESS;
+			}
+			EndPaint(Window, &Paint);
+			break;
+		}
+		default:
+		{
+			Result = DefWindowProc(Window, Message, WParam, LParam);
+			break;
+		}
+	}
+
+	return Result;
+}
+
 int CALLBACK WinMain(
 	HINSTANCE Instance,
 	HINSTANCE PrevInstance,
@@ -7,8 +70,60 @@ int CALLBACK WinMain(
 	int ShowCode
 )
 {
-	MessageBoxA(
-		0, "This is a message box", "Apocalypse", MB_OK | MB_ICONINFORMATION
-	);
+	WNDCLASS WindowClass = {};
+	WindowClass.lpfnWndProc = MainWindowCallback;
+	WindowClass.hInstance = Instance;
+	WindowClass.lpszClassName = "ApocalypseWindowClass";
+
+	if(RegisterClassA(&WindowClass))
+	{
+		HWND WindowHandle = CreateWindowExA(
+			0,
+			WindowClass.lpszClassName,
+			"Apocalypses",
+			WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			0,
+			0,
+			Instance,
+			0
+		);
+		if(WindowHandle)
+		{
+			GlobalRunning = true;
+			while(GlobalRunning)
+			{
+				MSG Message = {};
+				BOOL MessageResult = GetMessageA(&Message, 0, 0, 0);
+				if(MessageResult)
+				{
+					TranslateMessage(&Message);
+					DispatchMessageA(&Message);
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+		else
+		{
+			// TODO: Put this in a more unified logging location and get more 
+			// CONT: info
+			OutputDebugStringA("Failed to get window handle\n");
+			goto end;
+		}
+	}
+	else
+	{
+		// TODO: Put this in a more unified logging location and get more info
+		OutputDebugStringA("Failed to register window class\n");
+		goto end;
+	}
+
+end:
 	return 0;
 }
